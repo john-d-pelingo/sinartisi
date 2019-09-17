5d0aa779175857b0f9dba9d31acdbca5b999233d
John Darryl <johndarrylpelingo@gmail.com>
2019-09-17 23:27:06 +0200

    Fix test lint

--- color/color_test.go
+++ color/color_test.go
@@ -9,16 +9,16 @@ type rgbColorNumberPair struct {
 	result int
 }
 
-var rgbColorNumberTests = []rgbColorNumberPair{
-	{&RGB{0x55, 0x12, 0xAF}, 0x5512AF},
-	{&RGB{0x0, 0, 0x9}, 0x000009},
-	{&RGB{0x1, 0x2, 0x3}, 0x010203},
-	{&RGB{0, 0, 0}, 0},
-	{&RGB{255, 255, 255}, 0xFFFFFF},
-}
-
 func TestRGBColorNumber(t *testing.T) {
-	for _, pair := range rgbColorNumberTests {
+	var tests = []rgbColorNumberPair{
+		{&RGB{0x55, 0x12, 0xAF}, 0x5512AF},
+		{&RGB{0x0, 0, 0x9}, 0x000009},
+		{&RGB{0x1, 0x2, 0x3}, 0x010203},
+		{&RGB{0, 0, 0}, 0},
+		{&RGB{255, 255, 255}, 0xFFFFFF},
+	}
+
+	for _, pair := range tests {
 		got := RGBColorNumber(pair.input)
 		want := pair.result
 
